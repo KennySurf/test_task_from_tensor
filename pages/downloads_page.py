c404cf06d27c2ed4da1f14498455b1b5487b356f
Kenny <nzktime@gmail.com>
2024-02-23 22:18:00 +0300

    added downloads page

--- pages/downloads_page.py
+++ pages/downloads_page.py
@@ -11,19 +11,19 @@ class DownloadsPage(BasePage):
         self.click_element(DownloadsPageLocators.DOWNLOADS_LINK)
 
     def check_downloaded_plugin_on_pc(self, timeout=30):
-        download_path = os.path.join(os.getcwd(), 'downloads_folder')
+        download_path = os.path.join(os.path.dirname(__file__), '..' + '/downloads_folder')
         file_extension = ".crdownload"
         file_name = 'sbisplugin-setup-web.exe'
         download_complete = False
         end_time = time.time() + timeout
 
         while not download_complete and time.time() < end_time:
+            time.sleep(1)
             files = [f for f in os.listdir(download_path) if f.endswith(file_extension) or f == file_name]
             download_complete = bool(files)
-            time.sleep(1)
 
         if files:
-            file_size = os.path.getsize(os.path.join(download_path, files[-1]))
+            file_size = os.path.getsize(os.path.join(download_path, files[0]))
             return file_size
         else:
             return 0
